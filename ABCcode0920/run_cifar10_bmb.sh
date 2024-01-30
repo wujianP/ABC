82152f8f4e98863278607d43b7951afcb9222de7
wujianP <1051233700@qq.com>
2024-01-30 21:00:58 +0800

    test

--- ABCcode0920/run_cifar10_bmb.sh
+++ ABCcode0920/run_cifar10_bmb.sh
@@ -2,23 +2,23 @@ cd /discobox/wjpeng/code/2024/ImbalancedSSL/ABC/ABCcode0920
 conda activate /discobox/wjpeng/env/bmb
 git pull
 
-NAME=ABC_cifar100_r10_tcp-size128-get64-bp1-sp1-wt1
+NAME=ABC_cifar10_r20_tcp-size128-get64-bp2-sp1-wt1
 FOLDERDIR=/discobox/wjpeng/ckp/BMB/rebuttal/combine/$NAME
 mkdir $FOLDERDIR
 
 python ABCfix.py \
---gpu 3 \
---num_max_l 150 \
---num_max_u 300 \
---dataset cifar100 \
---imb_ratio 10 \
+--gpu 7 \
+--num_max_l 1500 \
+--num_max_u 3000 \
+--dataset cifar10 \
+--wandb_project_name ABC_BMB_CIFAR10 \
+--imb_ratio 20 \
 --epoch 500 \
 --val-iteration 500 \
 --out $FOLDERDIR \
 --pool_size 128 \
 --get_num 64 \
---bp_power 1 \
+--wandb_name $NAME \
+--bp_power 2 \
 --sp_power 1 \
---bmb_loss_wt 1 \
---wandb_project_name ABC_BMB_CIFAR100 \
---wandb_name $NAME
+--bmb_loss_wt 1
